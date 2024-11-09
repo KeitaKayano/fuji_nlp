f5319d74d020a96b95ae4b0875846da3f0e0dab7
KeitaKayano <keitaku0@gmail.com>
2024-11-09 22:01:57 +0900

    fix: モデルの保存場所の指定方法の修正

--- src/create_model.py
+++ src/create_model.py
@@ -12,7 +12,7 @@ def main():
         model = Word2Vec(sentences=dataset, vector_size=vector_size, sg=0)
 
         # モデルの保存
-        model.wv.save_word2vec_format(f"/workspace/src/custom_model/word2vec_CBOW_wiki_{vector_size}.bin", binary=True)
+        model.wv.save_word2vec_format(f"/workspace/src/custom_model/word2vec_CBOW_{dataset_name}_{vector_size}.bin", binary=True)
 
     for vector_size in [10, 50, 100, 200, 300, 500, 700, 1000]:
         # skip-gramモデルの作成とトレーニング
